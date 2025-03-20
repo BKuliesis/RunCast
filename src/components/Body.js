5b0e870ba693d9c8a5580ad444928e26a6f9b776
ec23381 <ec23381@qmul.ac.uk>
2025-03-20 18:28:30 +0000

    Added clothing recommendation. Does not implement API yet and need to add icons

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import styles from "./Body.module.css";
 import Header from "./body_components/Header";
 import WeatherToday from "./body_components/WeatherToday";
@@ -8,6 +9,22 @@ function Body({ weather }) {
             <Header weather={weather} />
             <WeatherToday weather={weather} />
         </div>
+=======
+import React from 'react';
+import Clothing from './Clothing';
+
+function Body() {
+
+    //Temporary test before api working
+    const weatherData = {
+        temperature: -10,
+        isRaining: true,
+    };
+    return (
+        <>
+        <Clothing temperature = {weatherData.temperature} isRaining = {weatherData.isRaining} />
+        </>
+>>>>>>> 15c8c0d (Added clothing recommendation. Does not implement API yet and need to add icons)
     );
 }
 
