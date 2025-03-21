66ccafca013d1b4a152bc4da95d590f32ba51b79
BKuliesis <bkuliesis@gmail.com>
2025-03-21 20:20:49 +0000

    Copied current state of header

--- src/components/body_components/Header.js
+++ src/components/body_components/Header.js
@@ -1,5 +1,16 @@
 function Header({ weather }) {
-    return null;
+    return (
+        <div className={styles.panel}>
+            <h2>Current Weather</h2>
+            <p>{currentWeather.name}, {currentWeather.sys.country}</p>
+            <p>{currentWeather.main.temp}°C</p>
+            <p>{currentWeather.weather[0].description}</p>
+            <img
+                src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`}
+                alt={currentWeather.weather[0].description}
+            />
+        </div>
+    );
 }
 
 export default Header;
