69477bf78f1774c0142bc549e2a3b322d9961991
BKuliesis <bkuliesis@gmail.com>
2025-03-22 15:51:05 +0000

    Header panel complete

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,11 @@
+import styles from "./Body.module.css";
 import Header from "./body_components/Header";
 
 function Body({ weather }) {
     return (
-        <>
+        <div className={styles.body}>
             <Header weather={weather} />
-        </>
+        </div>
     );
 }
 
