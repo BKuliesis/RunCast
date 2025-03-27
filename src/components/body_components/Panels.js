bf1423931f2827a1b7c8233295e8078f7cacc29a
D-varias <d.laav@yahoo.com>
2025-03-27 13:19:06 +0000

    finalised text stylings for forecast panels

--- src/components/body_components/Panels.js
+++ src/components/body_components/Panels.js
@@ -74,12 +74,12 @@ function Panels ({ forecast }) {
                     {forecast.list.slice(0, 8).map((hour, index) => (
                     <div className="panelListItem" key={index}>
                         <p className={`panelCell ${index === 0 ? "bold" : ""}`}>
-                            {new Date(hour.dt * 1000).toLocaleTimeString([], {
+                            {index === 0 ? 'Now' : new Date(hour.dt * 1000).toLocaleTimeString([], {
                                 hour: '2-digit',
                                 minute: '2-digit' 
                             })}
                         </p>
-                        <p className={`panelCell ${index === 0 ? "bold" : ""}`}>{hour.main.temp}°</p>
+                        <p className={`panelCell ${index === 0 ? "bold" : ""}`} id="tempNo">{hour.main.temp}°</p>
                         <img
                             src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`}
                             alt={hour.weather[0].description}
@@ -99,8 +99,11 @@ function Panels ({ forecast }) {
                     .filter((_, i) => i % 8 === 0)
                     .map((day, index) => (
                     <div className="panelListItem" key={index}>
-                        <p className={`panelCell ${index === 0 ? "bold" : ""}`}>{new Date(day.dt * 1000).toLocaleTimeString()}</p>
-                        <p className={`panelCell ${index === 0 ? "bold" : ""}`}>{day.main.temp}°</p>
+                        <p className={`panelCell ${index === 0 ? "bold" : ""}`}>
+                            {index === 0 ? 'Today' : new Date(day.dt * 1000)
+                            .toLocaleDateString('en-UK', {weekday: 'short', day: 'numeric'})}
+                        </p>
+                        <p className={`panelCell ${index === 0 ? "bold" : ""}`} id="tempNo">{day.main.temp}°</p>
                         <img
                             src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
                             alt={day.weather[0].description}
